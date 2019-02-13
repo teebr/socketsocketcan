9e869b9bf9decdf16e8510049e570cc7c45bae16
Kristian Sloth Lauszus <lauszus@gmail.com>
2019-02-14 00:16:28 +0100

    Fixed send method signature, super call and socket listen

--- socketsocketcan/tcpbus/server.py
+++ socketsocketcan/tcpbus/server.py
@@ -5,14 +5,16 @@ from threading import Thread
 import can
 from time import sleep
 
+
 class TCPBus(can.BusABC):
     RECV_FRAME_SZ = 21
     CAN_EFF_FLAG = 0x80000000
     CAN_RTR_FLAG = 0x40000000
     CAN_ERR_FLAG = 0x20000000
-    
-    def __init__(self,port,hostname="",can_filters=None,**kwargs):
-        super().__init__("whatever",can_filters)
+
+    def __init__(self, port, hostname="", can_filters=None, **kwargs):
+        super(TCPBus, self).__init__('tcpbus', can_filters=can_filters, **kwargs)
+
         self.hostname = hostname #TODO: check validity. ping?
         self.port = port #TODO: check validity.
         self._is_connected = False
@@ -23,7 +25,7 @@ class TCPBus(can.BusABC):
         #open socket and wait for connection to establish.
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.bind((self.hostname, self.port))
-        self._socket.listen()
+        self._socket.listen(1)
         self._conn, addr = self._socket.accept()
         self._is_connected = True
         self._conn.settimeout(0.5) #blocking makes exiting an infinite loop hard
@@ -42,14 +44,14 @@ class TCPBus(can.BusABC):
         except QueueEmpty:
             return None, False
 
-    def send(self, msg):
+    def send(self, msg, timeout=None):
         if msg.is_extended_id:
             msg.arbitration_id |= self.CAN_EFF_FLAG
         if msg.is_remote_frame:
             msg.arbitration_id |= self.CAN_RTR_FLAG
         if msg.is_error_frame:
-            msg.arbitration_id |= self.CAN_ERR_FLAG        
-        self.send_buffer.put(msg)
+            msg.arbitration_id |= self.CAN_ERR_FLAG
+        self.send_buffer.put(msg, timeout=timeout)
 
     def _stop_threads(self):
         self._shutdown_flag.put(True)
@@ -88,7 +90,7 @@ class TCPBus(can.BusABC):
             arb_id = can_id & 0x1FFFFFFF
         else:
             arb_id = can_id & 0x000007FF
-        
+
         return can.Message(
             timestamp = ts,
             arbitration_id = arb_id,
@@ -112,14 +114,14 @@ class TCPBus(can.BusABC):
                 except OSError:
                     # socket's been closed.
                     self._stop_threads()
-                    break                
+                    break
 
                 if len(data):
                     # process the 1 or more messages we just received
 
                     if len(part_formed_message):
                         data = part_formed_message + data #add on the previous remainder
-                    
+
                     #check how many whole and incomplete messages we got through.
                     num_incomplete_bytes = len(data) % self.RECV_FRAME_SZ
                     num_frames = len(data) // self.RECV_FRAME_SZ
@@ -156,7 +158,3 @@ class TCPBus(can.BusABC):
                         break
                 except QueueEmpty:
                     pass #NBD, just means nothing to send.
-
-
-
-        
